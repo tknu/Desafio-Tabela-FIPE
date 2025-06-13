3fe068c52614141a9681da4ca1480145fd46bead
Thiago Ueda <thiagokoiti@gmail.com>
2025-06-13 07:28:55 -0300

    Adiciona a funcionalidade de pesquisar as marcas do veículo selecionado no menu

--- src/main/java/br/com/alura/TabelaFipe/Principal/Principal.java
+++ src/main/java/br/com/alura/TabelaFipe/Principal/Principal.java
@@ -1,9 +1,15 @@
 package br.com.alura.TabelaFipe.Principal;
 
+import br.com.alura.TabelaFipe.Service.ConsumoApi;
+
 import java.util.Scanner;
 
 public class Principal {
     private Scanner leitura = new Scanner(System.in);
+
+    private ConsumoApi consumo = new ConsumoApi();
+
+    private final String URL_BASE = "https://parallelum.com.br/fipe/api/v1";
 
     public void exibeMenu() {
         var menu = """
@@ -16,6 +22,19 @@ public class Principal {
                 """;
         System.out.println(menu);
         var opcao = leitura.nextLine();
-        String endereco;
+        String endereco = "";
+
+        if (opcao.equalsIgnoreCase("carro")) {
+            endereco = URL_BASE + "/carros/marcas/";
+        } else if (opcao.equalsIgnoreCase("moto")) {
+            endereco = URL_BASE + "/motos/marcas/";
+        } else if (opcao.equalsIgnoreCase("caminhão")) {
+            endereco = URL_BASE + "/caminhoes/marcas/";
+        } else {
+            System.out.println("Opção inválida");
+        }
+
+        var json = consumo.obterDados(endereco);
+        System.out.println(json);
     }
 }
